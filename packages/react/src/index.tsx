bd42f1da99596ad967179ec87565683298da5c74
Mateus Garcia <mateussk83@gmail.com>
2023-04-03 21:25:00 -0300

    feat: insert stitches in react package

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,10 @@
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '',
+})
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+}
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,32 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+
+import { createStitches } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
