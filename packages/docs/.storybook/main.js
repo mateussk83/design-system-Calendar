9afe4a38059997f8c9cd61d2ed369bebd7354489
mateussk83 <mateussk83@gmail.com>
2023-04-16 14:51:23 -0300

    feat: insert colors in storybook documentation

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,7 +1,7 @@
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
   stories: [
-    '../src/stories/**/*.stories.mdx',
+    '../src/pages/**/*.stories.mdx',
     '../src/stories/**/*.stories.tsx',
   ],
   addons: [
