a4f4b6193f6867f6bf00edb0563fdbdf0989043e
mateussk83 <mateussk83@gmail.com>
2023-04-17 08:02:08 -0300

    feat: insert pages tokens in documentation

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,19 +1,19 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { Button, ButtonProps } from '@mateus-ignite-ui/react';
+import { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@mateus-ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
 
   args: {
     children: 'Enviar',
   },
-} as Meta<ButtonProps>;
+} as Meta<ButtonProps>
 
-export const Primary: StoryObj<ButtonProps> = {};
+export const Primary: StoryObj<ButtonProps> = {}
 
 export const Big: StoryObj<ButtonProps> = {
   args: {
-    size: 'big'
+    size: 'big',
   },
-};
+}
