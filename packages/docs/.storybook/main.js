a2483b8a11975b31b18e3dacce3f7948221c497d
mateussk83 <mateussk83@gmail.com>
2023-04-21 13:39:48 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -25,7 +25,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system-IgniteCalendar/'
+      config.base = '/design-system-Calendar/'
     }
 
     return config
